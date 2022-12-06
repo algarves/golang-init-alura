c2e27240d5fe9ce17f785d828fe9d6bbc5dbb27b
Khristian Algarves <khris@ueby.com>
2022-12-05 23:34:02 -0300

    feat(conditionals,instructions,arrays): Adiciona itens dos novos capítulos abordados nos capítulos: 3, 4 e 5 do curso

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"reflect"
+	"time"
 )
+
+const loop = 2
+const timeout = 5
 
 func main() {
 	hello()
@@ -45,13 +51,83 @@ func variablesEx2() {
 }
 
 func command() {
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
+	fmt.Println("1 - Start Monitoring")
+	fmt.Println("2 - Log Viewer")
+	fmt.Println("0 - Exit")
 
-	var cmd int
-	fmt.Scan(&cmd)
+	cmd := readCommand()
 
 	fmt.Println("The memory address of the cmd variable is:", &cmd)
 	fmt.Println("The input value of the cmd variable is:", cmd)
+
+	fmt.Println("Using conditional:")
+
+	if cmd == 1 {
+		startMonitoring()
+	} else if cmd == 2 {
+		fmt.Println("Logging ...")
+	} else if cmd == 0 {
+		fmt.Println("Exit")
+		exit()
+	} else {
+		fmt.Println("Command not found.")
+		os.Exit(-1)
+	}
+
+	fmt.Println("Using switch:")
+
+	switch cmd {
+	case 1:
+		startMonitoring()
+	case 2:
+		fmt.Println("Logging ...")
+	case 0:
+		fmt.Println("Exit")
+		exit()
+	default:
+		fmt.Println("Command not found.")
+		os.Exit(-1)
+	}
+}
+
+func readCommand() int {
+	var cmd int
+	fmt.Scan(&cmd)
+
+	return cmd
+}
+
+func exit() {
+	os.Exit(0)
+}
+
+func startMonitoring() {
+	fmt.Println("Monitoring...")
+	sites := []string{"https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	fmt.Println("Using FOR ...")
+	for i := 0; i < len(sites); i++ {
+		healthCheck(sites[i], i)
+	}
+
+	fmt.Println("")
+
+	fmt.Println("Using FOREACH ...")
+	for i := 0; i < loop; i++ {
+		for i, site := range sites {
+			healthCheck(site, i)
+		}
+		time.Sleep(timeout * time.Second)
+		fmt.Println("")
+	}
+}
+
+func healthCheck(site string, i int) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Endpoint", i, site, "is UP")
+	} else {
+		fmt.Println("Endpoint", i, site, "is DOWN")
+	}
 }
